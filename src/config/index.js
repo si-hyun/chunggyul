7e0d47426793d5e592f709c9108897a4035d1224
iamyoojincha <iamyoojincha@gmail.com>
2022-06-28 01:27:23 +0900

    add .gitignore

--- src/config/index.js
+++
@@ -1,3 +0,0 @@
-const API_BASE_URL = "http://localhost:9999/greenmandarine";
-
-export { API_BASE_URL };
--- src/pages/LoanProducts.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <card class="col-4" title="대출 가능한 상품">
-      <div>sadas</div>
-    </card>
-  </div>
-</template>
-<script></script>
-<style></style>
